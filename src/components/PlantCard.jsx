92276be9c053a8c603982cd30bcb9160a5f22f0f
Code Gaby <webigabriel@gmail.com>
2026-05-14 11:16:41 +0300

    My first commit

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const [inStock, setInStock] = useState(true);
+
+  function handleStockToggle() {
+    setInStock((prevStock) => !prevStock);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {inStock ? (
+        <button className="primary" onClick={handleStockToggle}>
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStockToggle}>Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plants={plants} setPlants={setPlants} />
+      <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
